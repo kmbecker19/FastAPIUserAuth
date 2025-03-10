0bbb8de1204f6e8e316bddfc76bcd8f7c5c54f7f
Kyle Becker <kmb08625@uga.edu>
2025-03-10 16:45:50 -0400

    Rename User to UserBase and UserDB to User

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sqlmodel import create_engine, Session, SQLModel, select
 from fastapi import FastAPI, HTTPException, Depends, Query, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-from models import User, UserDB, UserPublic, UserUpdate, UserCreate
+from models import User, UserPublic, UserUpdate, UserCreate
 
 # Set up SQL Engine
 db_path = Path().absolute() / 'database.db'
@@ -33,8 +33,8 @@ SessionDep = Annotated[Session, Depends(get_session)]
 
 TokenDep = Annotated[str, Depends(oath2_scheme)]
 
-async def get_current_user(token: TokenDep, session: SessionDep):
-    user = session.get(UserDB, token)
+async def get_current_user(token: TokenDep, session: SessionDep) -> User:
+    user = session.get(User, token)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -43,12 +43,12 @@ async def get_current_user(token: TokenDep, session: SessionDep):
         )
     return user
 
-UserDep = Annotated[UserDB, Depends(get_current_user)]
+UserDep = Annotated[User, Depends(get_current_user)]
 FormDep = Annotated[OAuth2PasswordRequestForm, Depends()]
 
 @app.post('/token')
 async def login(form_data: FormDep, session: SessionDep):
-    user = session.get(UserDB, form_data.username)
+    user = session.get(User, form_data.username)
     if not user or not user.hashed_password == form_data.password:
         raise HTTPException(status_code=400, detail='Invalid username or password')
     return {'access_token': user.username, 'token_type': 'bearer'}
@@ -56,7 +56,7 @@ async def login(form_data: FormDep, session: SessionDep):
 # Routes
 @app.post('/users', response_model=UserPublic)
 def create_user(user: UserCreate, session: SessionDep):
-    user_db = UserDB.model_validate(user)
+    user_db = User.model_validate(user)
     session.add(user_db)
     session.commit()
     session.refresh(user_db)
@@ -66,11 +66,11 @@ def create_user(user: UserCreate, session: SessionDep):
 def read_users_me(current_user: UserDep):
     return current_user
 
-@app.get('/users', response_model=list[UserDB])
+@app.get('/users', response_model=list[User])
 def read_users(session: SessionDep,
                offset: int = 0,
                limit: Annotated[int, Query(le=100)] = 100):
-    users = session.exec(select(UserDB).offset(offset).limit(limit)).all()
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
     if not users:
         raise HTTPException(status_code=404, detail='No users found')
     return users
--- models.py
+++ models.py
@@ -1,21 +1,21 @@
 from sqlmodel import Field, SQLModel
 
 # User Models
-class User(SQLModel):
+class UserBase(SQLModel):
     username: str | None = Field(primary_key=True)
     email: str | None = Field(default=None)
     full_name: str | None = Field(default=None)
 
-class UserDB(User, table=True):
+class User(UserBase, table=True):
     hashed_password: str
 
-class UserPublic(User):
+class UserPublic(UserBase):
     username: str
 
-class UserCreate(User):
+class UserCreate(UserBase):
     hashed_password: str
 
-class UserUpdate(User):
+class UserUpdate(UserBase):
     username: str | None = None
     email: str | None = None
     full_name: str | None = None
